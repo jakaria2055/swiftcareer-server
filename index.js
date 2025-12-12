0bb9de48c02c9ed52121603677d07298466cfeee
Jakaria Ahmed <jakaria88520@gmail.com>
2025-12-12 21:09:12 +0600

    path resolve

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: ["http://localhost:5173"],
+  origin: ["https://swiftcareer-client.vercel.app"],
   credentials: true,
 };
 
